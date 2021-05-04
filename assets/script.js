333aa7e0ce0c051b22d2b0116e76fa4d36dbc264
Ronald Main <rbmain84@yahoo.com>
2021-05-04 14:09:27 -0500

    update files

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,11 @@
-var quizQuestions = 
-[
-{
-question1: "You've got an hour to spend in the bookstore/library, where do you start?",
-    choiceA: "The classic! They’re always the best",
-    choiceB: "The scary dark section...obviously the thriller section",
-    choiceC: "The romance section",
-    choiceD: "Looking at facts from the past",
-    choiceE:"Magic is it duh! Fantasy section"
-},
-{
-question2: "If you could have a superpower what would it be? To...",
-    choiceA: "Travel back in time",
-    choiceB: "Spit random history facts",
-    choiceC: "Become super clever",
-    choiceD: "Put on a cloak of invisibility",
-    choiceE:"Make people fall in love, like cupid"
-},
-{
-question3: "Quick! A disaster is ahead of you. Use your imagination, how do you stop this disaster?",
-    choiceA: "You use quick wit and clues you gathered to address and stop the issue.",
-    choiceB: "You use your time machine and go back to stop the disaster.",
-    choiceC: "Love conquers all",
-    choiceD: "Get the fairies on your side to take down the disaster.",
-    choiceE:"Run!!!"
-},
-{
-question4: "Which word describes you the best?",
-    choiceA: "Serious",
-    choiceB: "Different",
-    choiceC: "Passionate",
-    choiceD: "Loyal",
-    choiceE:"Techy"
-},
-{
-question5: "You're a famous writer. Where would you choose to research your latest novel?",
-    choiceA: "The pyramids",
-    choiceB: "Within sight of the Northen Lights",
-    choiceC: "To the heart of battle/war",
-    choiceD: "Paris in the springtime",
-    choiceE:"The deepest. most unexplored part of the Amazon Rainforest"
-},
-{
-question6: "If you could choose a picture to hang in your room, it would be a picture of?",
-    choiceA: "Somthing scary or dangerous",
-    choiceB: "Fairies",
-    choiceC: "Picture of your favorite scientist",
-    choiceD: "Robots!",
-    choiceE:"Classic book quotes"
-},
-{
-question7: "If you could choose a picture to hang in your room, it would be a picture of?",
-        choiceA: "👽",
-        choiceB: "🕵️",
-        choiceC: "🤡",
-        choiceD:"🦖",
-        choiceE:"💖"
-},
-];
+function getbooks(){
+    document.getElementById('output').innerHTML="";
+    fetch("http://openlibrary.org/search.json?q="+document.getElementById("input").value)
+    .then(a => a.json())
+    .then(response =>{
+        console.log(response);
+      for(var i=0; i<10; i++){
+        document.getElementById("output").innerHTML+="<h2>"+response.docs[i].title+"</h2>"+response.docs[i].author_name[0]+"<br><img src='http://covers.openlibrary.org/b/isbn/"+response.docs[i].isbn[0]+"-M.jpg'><br>";
+      }
+    });
+  };

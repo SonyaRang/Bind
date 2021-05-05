5dc45bcd14572c9e2d9b36b179061c66a6615ec6
Sonz <sonyarangraj@msn.com>
2021-05-05 17:07:01 -0500

    changes

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ function getbooks(){
     .then(response =>{
         console.log(response);
     for(var i=0; i<10; i++){
-        document.getElementById("output").innerHTML+="<h2>"+response.docs[i].title+"</h2>"+response.docs[i].author_name[0]+"<br><img src='http://covers.openlibrary.org/b/isbn/"+response.docs[i].isbn[0]+"-M.jpg'><br>";
+        document.getElementById("output").innerHTML+="<br>TITLE:"+"<h2>"+response.docs[i].title+"</h2>"+"<br>AUTHOR:<br>"+response.docs[i].author_name[0]+"<br><img src='http://covers.openlibrary.org/b/isbn/"+response.docs[i].isbn[0]+"-M.jpg'><br>"+"<br>ISBN:<br>"+response.docs[i].isbn[0];
     }
     });
 };
